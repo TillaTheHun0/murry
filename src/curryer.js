2eef769291014047829c0a3cb286754220629498
Tyler Hall <0tillathehun0@gmail.com>
2018-12-19 11:56:38 -0500

    fix: throw error if extractor does not return an array

--- src/curryer.js
+++ src/curryer.js
@@ -1,22 +1,26 @@
 
-const { curry } = require('ramda')
+import { curry } from 'ramda'
 
-const curryer = curry((errorHandler, extractor, responder, controller) => {
-  return async (req, res, next) => {
-    try {
-      // extract the payload from the req
-      let payload = extractor(req)
-      payload = Array.isArray(payload) ? payload : [payload]
+const curryer = curry(
+  (errorHandler, extractor, responder, controller) =>
+    async (req, res, next) => {
+      try {
+        // extract the payload from the req
+        let payload = extractor(req)
 
-      // call the controller method providing the payload
-      let data = await controller(...payload)
-      responder(req, res, next)(data)
-      return data
-    } catch (err) {
-      errorHandler(req, res, next)(err)
+        if (!Array.isArray(payload)) {
+          throw new Error('Payload from extractor must be an array of arguments to spread into the controller')
+        }
+
+        // call the controller method providing the payload
+        let data = await controller(...payload)
+        responder(req, res, next)(data)
+        return data
+      } catch (err) {
+        errorHandler(req, res, next)(err)
+      }
     }
-  }
-})
+)
 
 module.exports = {
   curryer
