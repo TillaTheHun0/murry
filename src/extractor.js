92ea921528d17b1702578f9e20148cbb48ee7000
Tyler Hall <0tillathehun0@gmail.com>
2018-12-27 18:09:19 -0500

    fix: extractors to return arrays

--- src/extractor.js
+++ src/extractor.js
@@ -1,8 +1,8 @@
 
-const bodyExtractor = req => req.body
-const paramExtractor = req => req.params
-const queryExtractor = req => req.query
-const passthroughExtractor = req => req
+const bodyExtractor = req => [req.body]
+const paramExtractor = req => [req.params]
+const queryExtractor = req => [req.query]
+const passthroughExtractor = req => [req]
 
 export {
   bodyExtractor,
--- src/index.js
+++ src/index.js
@@ -12,36 +12,36 @@ const { jsonRes, statusRes } = responders
 const { paramExtractor, bodyExtractor, queryExtractor } = extractors
 
 // default error handling
-const defaultCurryer = curryer(passthroughHandler)
+const defaultMurryer = curryer(passthroughHandler)
 
-// default json response
-const defaultJsonResCurryer = defaultCurryer(__, jsonRes)
+// default json response. Placeholding extractor and controller
+const defaultJsonResCurryer = defaultMurryer(__, jsonRes)
 
 // extracts params and body from request and json response
 const postCurryer = defaultJsonResCurryer(req => {
   let params = paramExtractor(req)
   let body = bodyExtractor(req)
-  return { params, body }
+  return [...params, ...body]
 })
 
 // extracts params and body from request and json response
 const putCurryer = defaultJsonResCurryer(req => {
   let params = paramExtractor(req)
   let body = bodyExtractor(req)
-  return { params, body }
+  return [...params, ...body]
 })
 
 // extracts params and query from request and json response
 const getCurryer = defaultJsonResCurryer(req => {
   let query = queryExtractor(req)
   let params = paramExtractor(req)
-  return { query, params }
+  return [...query, ...params]
 })
 
 // extracts params from request and status res
-const deleteCurryer = defaultCurryer(req => {
+const deleteCurryer = defaultMurryer(req => {
   let params = paramExtractor(req)
-  return { params }
+  return [...params]
 }, statusRes)
 
 export {
@@ -49,7 +49,7 @@ export {
   errorHandlers,
   extractors,
   responders,
-  defaultCurryer,
+  defaultMurryer,
   postCurryer,
   putCurryer,
   getCurryer,
